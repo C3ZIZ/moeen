956616234e7688d234de9a19123c0e6ee14533fc
Abdulaziz Hafiz <abdulazizhafez2004@gmail.com>
2026-01-03 15:59:44 +0300

    Add relation managers for tasks, comments, reminders, members, and projects
    FIMALENT

---
+++ app/Filament/Resources/Tasks/RelationManagers/RemindersRelationManager.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources\Tasks\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class RemindersRelationManager extends RelationManager
+{
+    protected static string $relationship = 'reminders';
+
+    public function form(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                DateTimePicker::make('runs_at')
+                    ->required(),
+                DateTimePicker::make('sent_at'),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('runs_at')
+            ->columns([
+                TextColumn::make('runs_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('sent_at')
+                    ->dateTime()
+                    ->sortable(),
+                TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
